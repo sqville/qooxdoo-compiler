08be4abfc29d518e4c7e1765b391873be86a1b26
sqville <chris.eskew@sqville.com>
2019-03-18 16:15:02 -0500

    Added wax-franklin to skeletons
    Added wax-franklin to skeletons

---
+++ tool/cli/templates/skeleton/wax-franklin/source/class/custom/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("wax.theme.Theme",
+{
+  meta :
+  {
+    color : wax.theme.Color,
+    decoration : wax.theme.Decoration,
+    font : wax.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : wax.theme.Appearance
+  }
+});
